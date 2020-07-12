2341f4b90ea579e6d495d4e889e0f87edb99f382
ysf lghou <yousseflghoumaigui@gmail.com>
2020-07-12 01:26:33 +0100

    Fix markdown code highlights
    This commit is taking from another blog using the same template: https://github.com/yjose/elazizi.com/commit/e2820d618ce0c071e9fe84bbd520f057ec157850

--- src/components/mdx/Code.js
+++ src/components/mdx/Code.js
@@ -1,40 +1,81 @@
 import React from 'react'
 import theme from 'prism-react-renderer/themes/oceanicNext'
 import Highlight, { defaultProps } from 'prism-react-renderer'
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+// import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 
-const Code = ({children, codeString, className = 'language-js', ...props }) => {
-  const language = className.replace(/language-/, '')
-  if (props['react-live']) {
-    return (
-      <LiveProvider code={children.trim()} theme={theme}>
-        <LiveEditor />
-        <LiveError />
-        <LivePreview />
-      </LiveProvider>
-    )
+import { css } from '@emotion/core'
+import { minXL } from '../../lib/breakpoints'
+
+const RE = /{([\d,-]+)}/
+
+const wrapperStyles = css`
+  overflow: auto;
+`
+
+const preStyles = css`
+  float: left;
+  min-width: 100%;
+  overflow: initial;
+`
+
+function calculateLinesToHighlight(meta) {
+  if (RE.test(meta)) {
+    const lineNumbers = RE.exec(meta)[1]
+      .split(',')
+      .map(v => v.split('-').map(y => parseInt(y, 10)))
+    return index => {
+      const lineNumber = index + 1
+      const inRange = lineNumbers.some(([start, end]) =>
+        end ? lineNumber >= start && lineNumber <= end : lineNumber === start,
+      )
+      return inRange
+    }
   } else {
-    return (
-      <Highlight
-        {...defaultProps}
-        code={children.trim()}
-        language={language}
-        theme={theme}
-      >
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={{ ...style, padding: '20px' }}>
+    return () => false
+  }
+}
+
+function Code({ codeString, language, metastring }) {
+  const shouldHighlightLine = calculateLinesToHighlight(metastring)
+  return (
+    <Highlight
+      {...defaultProps}
+      code={codeString}
+      language={language}
+      theme={theme}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <div css={wrapperStyles}>
+          <pre className={className} style={style} css={preStyles}>
             {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({ line, key: i })}>
+              <div
+                key={i}
+                {...getLineProps({
+                  line,
+                  key: i,
+                  className: shouldHighlightLine(i) ? 'highlight-line' : '',
+                })}
+              >
+                <span
+                  css={css`
+                    display: inline-block;
+                    width: 2em;
+                    user-select: none;
+                    opacity: 0.3;
+                  `}
+                >
+                  {i + 1}
+                </span>
                 {line.map((token, key) => (
                   <span key={key} {...getTokenProps({ token, key })} />
                 ))}
               </div>
             ))}
           </pre>
-        )}
-      </Highlight>
-    )
-  }
+        </div>
+      )}
+    </Highlight>
+  )
 }
 
 export default Code
